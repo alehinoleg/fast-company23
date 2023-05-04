557b28c6e04c8a797696687df37489126c241f05
Alehin Oleg <alehinoleg1@gmail.com>
2023-05-04 23:06:09 +0700

    5v1

--- src/components/users.jsx
+++ src/components/users.jsx
@@ -14,7 +14,7 @@ const Users = ({
     handleBookmark,
     renderPhrase
 }) => {
-    const pageSize = 6;
+    const pageSize = 4;
     const [currentPage, setCurrentPage] = useState(1);
     const [professions, setProfessions] = useState(api.professions.fetchAll());
     const [selectedProf, setSelectedProf] = useState();
